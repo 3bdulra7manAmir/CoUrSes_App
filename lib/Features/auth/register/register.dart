799f979b77dd64f51e0b37030fcffb999e7b1995
3bdulra7manamir <ethanhunt4d@gmail.com>
2025-01-18 23:54:17 +0200

    added 1. some consts 2. BlocBuilder & BlocBuilder 3. Validation Form 4. Cubit Obeject 5. After SignUp Moves to Login

--- lib/Features/auth/register/register.dart
+++ lib/Features/auth/register/register.dart
@@ -1,3 +1,5 @@
+// ignore_for_file: use_build_context_synchronously
+
 import 'package:courses_app/Core/constants.dart';
 import 'package:courses_app/Core/utils/app_router.dart';
 import 'package:courses_app/Core/utils/styles.dart';
@@ -6,151 +8,176 @@ import 'package:courses_app/Core/widgets/custom_column.dart';
 import 'package:courses_app/Core/widgets/custom_container.dart';
 import 'package:courses_app/Core/widgets/custom_text.dart';
 import 'package:courses_app/Core/widgets/custom_textfield.dart';
-import 'package:courses_app/Features/auth/login/firebase_login_auth_cubit/firebase_login_auth.dart';
+import 'package:courses_app/Features/auth/register/firebase_register_auth_cubit/firebase_register_auth_cubit.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:go_router/go_router.dart';
-
 
 class RegisterView extends StatefulWidget
 {
   const RegisterView({super.key});
 
   @override
-  State<RegisterView> createState() => _RegisterViewState();
+  State<RegisterView> createState() => RegisterViewState();
 }
 
-class _RegisterViewState extends State<RegisterView>
+class RegisterViewState extends State<RegisterView>
 {
+  final formKey = GlobalKey<FormState>();
 
   final TextEditingController emailController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
-  bool obscureText = true;
-  bool isChecked = false;
 
+  bool isChecked = false;
+  bool obscureText = true;
+
+  
+  //TO PREVENT Memory leak //Tharwat Samy...
+  @override
+  void dispose()
+  {
+    emailController.dispose();
+    passwordController.dispose();
+    super.dispose();
+  }
+  
   @override
   Widget build(BuildContext context)
   {
-    return SafeArea(
-      child: Scaffold(
-        body: Stack(
-          children:
-          [
-            //BOTTOM Container
-            CustomContainerBackGround(
-              containerChild: Padding(
-                padding: EdgeInsets.only(top: (KMediaQuery(context).height) * 0.07, left: KMediaQuery(context).width * 0.05,),
-                child: Align(
-                  alignment: Alignment.topLeft,
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children:
-                    [
-                      CustomTextWidget(widgetText: 'Sign Up', widgetTextStyle: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),),
-                      CustomTextWidget(widgetText: 'Enter your details below & free sign up', widgetTextStyle: Styles.textStyle14,),
-                    ],
-                  )
-                ),
-              ),
-            ),
-
-            //TOP Container
-            AfContainerBody(
-              topPercentage: 0.2,
-              positionedChild: CustomContainerBackGround(
-                containerDecoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10)),
-                ),
+    return BlocProvider(
+      create: (context) => FirebaseRegisterAuthCubit(),
+      child: BlocBuilder<FirebaseRegisterAuthCubit, FirebaseRegisterAuthStates>(
+        builder: (context, state)
+        {
+          var firebaseRCubit = BlocProvider.of<FirebaseRegisterAuthCubit>(context);
+          return SafeArea(
+            child: Scaffold(
+              body: Form(
+                key: formKey,
+                child: Stack(
+                  children:
+                  [
+                    //BOTTOM Container
+                    CustomContainerBackGround(
+                      containerChild: Padding(
+                        padding: EdgeInsets.only(top: (KMediaQuery(context).height) * 0.07, left: KMediaQuery(context).width * 0.05,),
+                        child: Align(
+                            alignment: Alignment.topLeft,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children:
+                              [
+                                CustomTextWidget(widgetText: 'Sign Up', widgetTextStyle: Styles.textStyle30.copyWith(fontWeight: FontWeight.bold),),
+                                CustomTextWidget(widgetText: 'Enter your details below & free sign up', widgetTextStyle: Styles.textStyle14,),
+                              ],
+                            )),
+                      ),
+                    ),
                 
-                containerChild: Padding(
-                  padding: EdgeInsets.only(top: (KMediaQuery(context).height) * 0.04, left: KMediaQuery(context).width * 0.015),
-                  child: Column(
-                    children:
-                    [
-                      CustomTextWidget(widgetText: 'Your Email',),
-                                
-                      CustomTextfield(
-                        fieldController: emailController,
-                        fieldTextInputType: TextInputType.emailAddress,
-                        fieldOnSubmitted: (string) {print(emailController.text);},
-                      ),
-                  
-                      SizedBox(height: 20,),
-                  
-                      CustomTextWidget(widgetText: 'Password',),
-                                    
-                      CustomTextfield(
-                        fieldController: passwordController,
-                        fieldObscureText: obscureText,
-                        fieldSuffixIcon: IconButton(icon: Icon(obscureText ? Icons.visibility_off : Icons.visibility,),
-                        onPressed: () {setState(() {obscureText = !obscureText;});},),
-                        fieldOnSubmitted: (string)
-                        {print(passwordController.text);},
-                      ),
-                  
-                      SizedBox(height: 30,),
-                  
-                      CustomBlueButton(buttonText: 'Creat account', buttonWidth: 0.9, buttonOnPressed: () async
-                        {
-                          print('Create Account Button Pressed');
-                          print("${emailController.text} \t ${passwordController.text}");
-
-                          await firebaseRegisterAuth(emailController.text, passwordController.text);
-                          
-                          emailController.clear();
-                          passwordController.clear();
-                        },
-                      ),
-              
-                      SizedBox(height: 20,),
-              
-                      Padding(
-                        padding: EdgeInsets.only(left: KMediaQuery(context).width * 0.02, right: KMediaQuery(context).width * 0.015),
-                        child: Row(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children:
-                          [
-
-                            Checkbox(value: isChecked, onChanged: (bool? value)
-                            {setState((){isChecked = value!;});},
-                            activeColor: Colors.blue[800],
-                            ),
-
-                            Flexible(
-                              child: Text("By creating an account you have to agree with our them & condication.",
-                              softWrap: true, // Ensures the text will wrap
-                              overflow: TextOverflow.visible, // Allows text to continue onto the next line
+                    //TOP Container
+                    AfContainerBody(topPercentage: 0.2,
+                      positionedChild: CustomContainerBackGround(
+                        containerDecoration: BoxDecoration(color: Colors.white,
+                          borderRadius: BorderRadius.only(topLeft: Radius.circular(10), topRight: Radius.circular(10)),),
+                        containerChild: Padding(
+                          padding: EdgeInsets.only(top: (KMediaQuery(context).height) * 0.04, left: KMediaQuery(context).width * 0.015),
+                          child: Column(
+                            children:
+                            [
+                              const CustomTextWidget(widgetText: 'Your Email',),
+                              CustomTextfield(fieldController: emailController, fieldTextInputType: TextInputType.emailAddress,
+                              fieldVaidator: Validation().validateEmail,
                               ),
-                            ),
+                              SizedBox(height: 20,),
+                
+                              const CustomTextWidget(widgetText: 'Password',),
+                              CustomTextfield(fieldController: passwordController, fieldObscureText: obscureText,
+                                fieldVaidator: Validation().validatePassword,
+                                fieldSuffixIcon: IconButton(icon: Icon(obscureText ? Icons.visibility_off : Icons.visibility,),
+                                  onPressed: () {setState(() {obscureText = !obscureText;});},
+                                ),
+                              ),
+                
+                              const SizedBox(height: 30,),
+                
+                              CustomBlueButton(buttonText: 'Creat account', buttonWidth: 0.9, buttonOnPressed: () async
+                              {
+                                Validation().submitForm(formKey, isChecked, context);
 
-                          ],
+                                print('Create Account Button Pressed');
+                                print("${emailController.text} \t ${passwordController.text}");
+                
+                                await firebaseRCubit.firebaseRegisterAuth(emailController.text, passwordController.text);
+                
+                                emailController.clear();
+                                passwordController.clear();
+
+                                setState(() => isChecked = false);
+
+                                Future.delayed(Duration(seconds: 1), () => GoRouter.of(context).go(AppRouter.kLoginView));
+                                },
+                              ),
+                
+                              const SizedBox(height: 20,),
+                
+                              Padding(
+                                padding: EdgeInsets.only(left: KMediaQuery(context).width * 0.02, right: KMediaQuery(context).width * 0.015),
+                                child: Row(
+                                  mainAxisAlignment: MainAxisAlignment.start,
+                                  children:
+                                  [
+                                    Checkbox(
+                                      value: isChecked,
+                                      onChanged: (bool? value)
+                                      {
+                                        setState(() => isChecked = value!);
+                                        print('Checkbox State: $value');
+                                      },
+                                      activeColor: Colors.blue[800],
+                                    ),
+                                    const Flexible(
+                                      child: Text("By creating an account you have to agree with our them & condication.",
+                                        softWrap: true, // Ensures the text will wrap
+                                        overflow: TextOverflow.visible, // Allows text to continue onto the next line
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ),
+                
+                              const SizedBox(height: 40,),
+                
+                              Row(
+                                mainAxisAlignment: MainAxisAlignment.center,
+                                children:
+                                [
+                                  CustomTextWidget(widgetText: 'Already have an account?', widgetTextStyle: TextStyle(fontSize: 13.sp,),),
+                                  GestureDetector(
+                                    onTap: ()
+                                    {
+                                      GoRouter.of(context).push(AppRouter.kLoginView);
+                                      print('WENT TO\tLOGIN_VIEW');
+                                    },
+                                    child: CustomTextWidget(widgetText: 'Log in', widgetTextStyle: TextStyle(fontSize: 14.sp, color: Color.fromRGBO(61, 93, 255, 1),
+                                        decoration: TextDecoration.underline,
+                                        fontWeight: FontWeight.w900,
+                                      ),
+                                    ),
+                                  ),
+                                ],
+                              ),
+                            ],
+                          ),
                         ),
                       ),
-
-                      SizedBox(height: 40,),
-
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.center,
-                        children:
-                        [
-                          CustomTextWidget(widgetText: 'Already have an account?', widgetTextStyle: TextStyle(fontSize: 13.sp,),),
-                          
-                          GestureDetector(
-                            onTap: (){GoRouter.of(context).push(AppRouter.kLoginView); print('WENT TO\tLOGIN_VIEW');},
-                            child: CustomTextWidget(widgetText: 'Log in',
-                            widgetTextStyle: TextStyle(fontSize: 14.sp, color:Color.fromRGBO(61, 93, 255, 1), decoration: TextDecoration.underline, fontWeight: FontWeight.w900,
-                            ),),
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
+                    ),
+                  ],
                 ),
               ),
             ),
-          ],
-        ),
+          );
+        },
       ),
     );
   }
