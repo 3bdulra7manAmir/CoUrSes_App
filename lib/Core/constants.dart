22d61b177632b6cd85035406d6faf12df6e8ffc9
3bdulra7manamir <ethanhunt4d@gmail.com>
2025-01-21 20:38:51 +0200

    adding random cards colors

--- lib/Core/constants.dart
+++ lib/Core/constants.dart
@@ -13,3 +13,14 @@ class KMediaQuery
 
 const String kGtSectraFine = 'GT Sectra Fine';
 const Color kButtonsBlueColor = Color.fromRGBO(61, 93, 255, 1);
+
+
+
+const Color kCardColorPinkContainer = Color(0xffFFE7EE);
+const Color kCardColorPinkLine = Color(0xffEC7B9C);
+
+const Color kCardColorBlueContainer = Color(0xffBAD6FF);
+const Color kCardColorBlueLine = Color(0xff3D5CFF);
+
+const Color kCardColorGreenContainer = Color(0xffBAE0DB);
+const Color kCardColorGreenLine = Color(0xff398A80);
