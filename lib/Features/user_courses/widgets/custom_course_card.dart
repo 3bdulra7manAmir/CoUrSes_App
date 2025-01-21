b4d1d97587ac25f0dac3ac646100fda1cb63a916
3bdulra7manamir <ethanhunt4d@gmail.com>
2025-01-21 20:39:45 +0200

    adding random functionality to the courses card

--- lib/Features/user_courses/widgets/custom_course_card.dart
+++ lib/Features/user_courses/widgets/custom_course_card.dart
@@ -1,20 +1,24 @@
 import 'package:courses_app/Core/constants.dart';
-import 'package:courses_app/Core/widgets/custom_linear_Indicator.dart';
+import 'package:courses_app/Core/utils/functions/random_card_color.dart';
+import 'package:courses_app/Core/widgets/custom_linear_indicator.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class CustomCourseCard extends StatelessWidget
 {
-  const CustomCourseCard({super.key,});
+  CustomCourseCard({super.key,});
+
+  final result = randomFunction();
 
   @override
-  Widget build(BuildContext context) {
+  Widget build(BuildContext context)
+  {
     return SizedBox(
       width: KMediaQuery(context).width * 0.45,
       height: 150.h,
               
       child: Card(
-        color: Color(0xffFFE7EE),
+        color: result.keys.first,
         child: Padding(
           padding: const EdgeInsets.all(12.0),
           child: Column(
@@ -22,13 +26,13 @@ class CustomCourseCard extends StatelessWidget
             children:
             [
               Text('Product\nDesign v1.0', style: TextStyle(fontWeight: FontWeight.bold, fontSize: 14.sp,
-              foreground: Paint()..style = PaintingStyle.stroke..strokeWidth = 1.2..color = Colors.black,
+              //foreground: Paint()..style = PaintingStyle.stroke..strokeWidth = 1.2..color = Colors.black,
               overflow: TextOverflow.ellipsis
               ),),
                         
               const SizedBox(height: 15,),
                         
-              CustomLinearIndicator(backgroundColor: Colors.white, progressColor: Colors.pink[200]!, percent: 67 / 100, width: 0.33,),
+              CustomLinearIndicator(backgroundColor: Colors.white, progressColor: result.values.first, percent: 67 / 100, width: 0.33,),
           
               const SizedBox(height: 10,),
           
@@ -43,13 +47,13 @@ class CustomCourseCard extends StatelessWidget
                       Text('Completed'),
               
                       Text('14/24', style: TextStyle(fontSize: 18.sp, fontWeight: FontWeight.bold,
-                      foreground: Paint()..style = PaintingStyle.stroke..strokeWidth = 1..color = Colors.black,),
-                      ),
+                      //foreground: Paint()..style = PaintingStyle.stroke..strokeWidth = 1..color = Colors.black,
+                      ),),
                     ],
                   ),
               
                   CircleAvatar(
-                    backgroundColor: Colors.pink[200],
+                    backgroundColor: result.values.first,
                     child: IconButton(onPressed: () {},
                       icon: const Icon(Icons.play_arrow_rounded),
                       color: Colors.white,
