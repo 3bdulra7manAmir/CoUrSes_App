d3d3374a9f27868d496216c03a67436396ce9131
3bdulra7manamir <ethanhunt4d@gmail.com>
2025-01-21 20:38:34 +0200

    random cards functionality

---
+++ lib/Core/utils/functions/random_card_color.dart
@@ -0,0 +1,47 @@
+// Function to return a linked value based on the input
+import 'dart:math';
+
+import 'package:courses_app/Core/constants.dart';
+import 'package:flutter/material.dart';
+
+Color getLinkedValue(Color cardBackgroundColor)
+{
+  switch (cardBackgroundColor)
+  {
+    case kCardColorPinkContainer:
+      return kCardColorPinkLine;
+    case kCardColorBlueContainer:
+      return kCardColorBlueLine;
+    case kCardColorGreenContainer:
+      return kCardColorGreenLine;
+    default:
+      throw ArgumentError('Invalid input. Expected a valid card background color.');
+  }
+}
+
+// Function to randomly select one of the three values and return it along with its linked value
+Map<Color, Color> randomFunction()
+{
+  final random = Random();
+
+  // Map integers to corresponding Color values
+  final colorOptions =
+  [
+    kCardColorPinkContainer,
+    kCardColorBlueContainer,
+    kCardColorGreenContainer,
+  ];
+
+  // Randomly select a Color from the list
+  final randomColor = colorOptions[random.nextInt(colorOptions.length)];
+
+  // Get the linked Color value
+  final linkedColor = getLinkedValue(randomColor);
+
+  return {randomColor: linkedColor};
+}
+
+
+
+// final result = randomFunction();
+// print('Random Background Color: ${result.keys.first}, Linked Line Color: ${result.values.first}');
