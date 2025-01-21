cd762931ee8d2c315be13a1169bb1d533ca4fe90
3bdulra7manamir <ethanhunt4d@gmail.com>
2025-01-21 17:05:15 +0200

    creating courses view

---
+++ lib/Features/user_courses/courses_view.dart
@@ -0,0 +1,39 @@
+import 'package:courses_app/Core/constants.dart';
+import 'package:courses_app/Features/user_courses/widgets/custom_course_card.dart';
+import 'package:courses_app/Features/user_courses/widgets/custom_page_header.dart';
+import 'package:courses_app/Features/user_courses/widgets/custom_progress_container.dart';
+import 'package:flutter/material.dart';
+
+class UserCoursesView extends StatelessWidget
+{
+  const UserCoursesView({super.key});
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return SafeArea(
+      child: Scaffold(
+        body: Padding(
+          padding: EdgeInsets.only(left: KMediaQuery(context).width * 0.05),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children:
+            [
+              CustomCoursesHeader(),
+          
+              const SizedBox(height: 50,),
+          
+              CustomProgressCard(),
+          
+              const SizedBox(height: 20,),
+          
+              CustomCourseCard(),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+
+
