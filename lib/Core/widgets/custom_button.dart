1453defbc46280c0a8355974f5a12e10b47408e7
3bdulra7manamir <ethanhunt4d@gmail.com>
2025-01-22 20:07:33 +0200

    adding Reversed Blue Button

--- lib/Core/widgets/custom_button.dart
+++ lib/Core/widgets/custom_button.dart
@@ -32,3 +32,33 @@ class CustomBlueButton extends StatelessWidget
     );
   }
 }
+
+class CustomReversedButton extends StatelessWidget
+{
+  const CustomReversedButton({super.key, required this.buttonWidth, this.buttonHeight, required this.buttonText, required this.buttonOnPressed,});
+
+  final double buttonWidth;
+  final String buttonText;
+  final void Function() buttonOnPressed;
+  final double? buttonHeight;
+
+  @override
+  Widget build(BuildContext context)
+  {
+    return SizedBox(
+      width: KMediaQuery(context).width * buttonWidth,
+      height: KMediaQuery(context).height * (buttonHeight ?? 0.06),
+      child: ElevatedButton(
+        style: ButtonStyle(
+          backgroundColor: WidgetStateProperty.all(Colors.white),
+          shape: WidgetStateProperty.all(RoundedRectangleBorder(
+            borderRadius: BorderRadius.circular(12.0.r),
+          ),
+        ),
+      ),
+      onPressed: buttonOnPressed,
+      child: Center(child: Text(buttonText, style: Styles.textStyle16.copyWith(color: Color(0xff3d5cff), fontWeight: FontWeight.bold),)),
+      ),
+    );
+  }
+}
